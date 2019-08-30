25d87807939e44437a6da5b4f9705ece05179a73
ibarakaiev <ibarakaiev@gmail.com>
2019-08-30 11:45:34 -0400

    Template ready

---
+++ popup/obj.js
@@ -0,0 +1,8 @@
+var policy = {
+    "name": "Facebook, Inc.",
+    "hostname": "facebook.com",
+    "slug": "facebook",
+    "score": 2.0,
+    "last_updated": "2019-08-25T19:26:14.126Z",
+    "has_warnings_active": true
+}
