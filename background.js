4e0d30c79e1597c6adb1d169391b16c1d1c663cf
R. Miles McCain <x1-key@sendmiles.email>
2019-08-30 10:28:29 -0400

    small updates

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-const API_URL = "https://localhost:3000/";
+// const API_URL = "https://localhost:3000/";
 
-browser.pageAction.onClicked.addListener(() => {
-    browser.pageAction.openPopup();
-});
-
+// browser.pageAction.onClicked.addListener(() => {
+//     browser.pageAction.openPopup();
+// });
